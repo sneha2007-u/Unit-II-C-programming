7347cfac761362d0cae448ec06c816cf15676cf2
sneha2007-u <snehaudatewar007@gmail.com>
2026-09-22 14:26:57 +0530

    Add basic single inheritance example
    Implement basic single inheritance with Person and Student classes.

---
+++ Concept 1: Basic Single Inheritance.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <utility>
+class Person {
+protected:
+ std::string name;
+public:
+ explicit Person(std::string personName) : name(std::move(personName)) {}
+ void displayName() const {
+ std::cout << "Name: " << name << '\n';
+ }
+};
+class Student : public Person {
+private:
+    int rollNumber;
+public:
+ Student(std::string studentName, int roll)
+ : Person(std::move(studentName)), rollNumber(roll) {}
+ void displayStudent() const {
+ displayName();
+ std::cout << "Roll Number: " << rollNumber << '\n';
+ }
+};
+int main() {
+ Student student("Sneha", 101);
+ student.displayStudent();
+ return 0;
+}
